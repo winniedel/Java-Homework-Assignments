8504d5f749d8de14d4274284638816a708c5048e
winniedel <109555641+winniedel@users.noreply.github.com>
2022-07-18 18:01:18 -0400

    Update Student.java

---
+++ HW 2/Student.java
@@ -0,0 +1,16 @@
+public class Student extends Person {
+
+    String university;
+
+
+    Student(String name, int age, String location, String university) {
+
+        super(name, age, location);
+        this.university = university;
+
+    }
+
+}
+
+
+
