9946f5900a011eec3955217402b082b134ccf527
winniedel <109555641+winniedel@users.noreply.github.com>
2022-07-18 17:59:17 -0400

    Update Person.java

---
+++ HW 2/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    String name;
+    int age;
+    String location;
+
+
+   Person(String name, int age, String location){
+       this.name = name;
+       this.age = age;
+       this.location = location;
+
+   }
+
+
+}
+
+
